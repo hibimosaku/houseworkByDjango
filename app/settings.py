78b8d763700dfeb74c4df380dc9a86141476f93c
hibimosaku <n.kubomitsu@gmail.com>
2022-08-09 04:39:44 +0900

    added host2

--- app/settings.py
+++ app/settings.py
@@ -18,7 +18,7 @@ DEBUG = config('DEBUG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['housework-restapi.herokuapp.com/']
+ALLOWED_HOSTS = ['housework-restapi.herokuapp.com']
 
 
 # Application definition
